7ec39c9825d37ba89c3bd777539e2344e57779ee
Fahd Mohammed <34211858+Fahdmoh01@users.noreply.github.com>
2022-01-11 19:39:58 +0000

    Time: 1 ms (61.32%), Space: 40.2 MB (5.97%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -1,27 +1,43 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         ArrayList <Boolean> list = new ArrayList<>();
-        for(int i = 0; i < candies.length; i++){
-            int temp = candies[i];
-            candies[i]= candies[i] + extraCandies;
-            int max = findMax(candies);
-            if(candies[i] == max){
+    //loop through array and find the maximum element.
+        int max = Integer.MIN_VALUE;
+        for( int candy: candies){
+            max= Math.max(candy, max);
+        }
+    //add the extraCandies to the elements and compare with the maximum
+        for(int candy: candies){
+            if(candy + extraCandies >= max){
                 list.add(Boolean.TRUE);
             }else{
                 list.add(Boolean.FALSE);
             }
-            candies[i]= temp;
         }
+        
         return list;
-    }
+        
+//         for(int i = 0; i < candies.length; i++){
+//             int temp = candies[i];
+//             candies[i]= candies[i] + extraCandies;
+//             int max = findMax(candies);
+//             if(candies[i] == max){
+//                 list.add(Boolean.TRUE);
+//             }else{
+//                 list.add(Boolean.FALSE);
+//             }
+//             candies[i]= temp;
+//         }
+//         return list;
+//     }
     
-    int findMax(int arr[]){
-     int maxNum = Integer.MIN_VALUE;
-        for(int i =0; i < arr.length; i++){
-            if(arr[i] >maxNum){
-                maxNum = arr[i];
-            }
-        }
-        return maxNum;
+    // int findMax(int arr[]){
+    //  int maxNum = Integer.MIN_VALUE;
+    //     for(int i =0; i < arr.length; i++){
+    //         if(arr[i] >maxNum){
+    //             maxNum = arr[i];
+    //         }
+    //     }
+    //     return maxNum;
     }
 }
