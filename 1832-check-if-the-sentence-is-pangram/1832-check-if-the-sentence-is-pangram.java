4c30a21b0c79f81fc815f92caadd7a32c3212fb6
Fahd Mohammed <34211858+Fahdmoh01@users.noreply.github.com>
2022-01-12 21:49:09 +0000

    Time: 2 ms (55.98%), Space: 39 MB (12.65%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        int [] chars = new int[26];
+//         int [] chars = new int[26];
       
-        for(char c :  sentence.toCharArray()){
-            chars[c -'a']++;
-        }
+//         for(char c :  sentence.toCharArray()){
+//             chars[c -'a']++;
+//         }
         
-        for(int num: chars){
-            if(num == 0) return false;
-        }
+//         for(int num: chars){
+//             if(num == 0) return false;
+//         }
         
-        return true;
+//         return true;
     
 //     int [] chars = new int[26];
 //     for(int i =0; i < sentence.length(); i++){
@@ -22,5 +22,17 @@ class Solution {
         
 //         return true;
 
+        boolean [] chars = new boolean[26];
+      
+        for(char c :  sentence.toCharArray()){
+            chars[c -'a'] = true;
+        }
+        
+        for(boolean checker: chars){
+            if(!checker) return false;
+        }
+        
+        return true;
+       
     }
 }
