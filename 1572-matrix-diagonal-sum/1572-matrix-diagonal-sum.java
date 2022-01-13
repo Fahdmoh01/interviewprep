5ebbd0ab34a25e2dec95efd5446f621c04723469
Fahd Mohammed <34211858+Fahdmoh01@users.noreply.github.com>
2022-01-13 18:13:39 +0000

    Time: 0 ms (100.00%), Space: 39 MB (62.91%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int totalSum = secondaryDiagSum(mat) + primeDiagSum(mat);
+        if(mat.length % 2 != 0){
+            int mid= mat.length /2;
+            totalSum = totalSum - mat[mid][mid];
+            return totalSum;
+        }
+        return totalSum;
+    }
+    
+    int secondaryDiagSum(int[][] arr){
+        int end = arr.length -1;
+        int secSum = 0;
+        while(end >= 0){
+            for(int row=0; row <arr.length; row++){
+                secSum += arr[row][end];   
+                end--;
+            }
+        }
+        return secSum;
+    }
+    
+    int primeDiagSum(int[][] arr){
+        int start = 0;
+        int primeSum = 0;
+        while( start < arr.length){
+        for(int row=0; row <arr.length; row++){
+                primeSum += arr[row][start];
+                start++;
+            }
+        }
+        return primeSum;
+    }
+}
