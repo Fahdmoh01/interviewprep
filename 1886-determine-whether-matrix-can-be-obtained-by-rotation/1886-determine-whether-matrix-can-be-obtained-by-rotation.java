345af2359bdec1374d95268f39f718e3c10bb605
Fahd Mohammed <34211858+Fahdmoh01@users.noreply.github.com>
2022-01-19 07:52:45 +0000

    Time: 3 ms (8.01%), Space: 39.4 MB (12.64%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,27 +1,46 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        /*
-            
-        */
-        for(int i =0; i<4; i++){
-            if(Arrays.deepEquals(mat,target))return true;
-            mat = rotateMatrix(mat);
+        for(int i =0; i< 4; i++){
+            if(Arrays.deepEquals(mat,target))
+                return true;
+            rotateMatrix(mat);
         }
+        
         return false;
     }
     
-    int[][] rotateMatrix(int[][] matrix){
-            int row = matrix.length;
-            int col = matrix[0].length;
-            int new_matrix[][] = new int[row][col];
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                new_matrix[j][row-1-i]= matrix[i][j];
-            }    
+    
+    void rotateMatrix(int[][] mat){
+/*new swap the values at each diagonal and swap the values in the columns/horizontals more like reverse colunmns/horizontals*/
+        int row = mat.length;
+        int col = mat[0].length;
+        //int n_matrix[][] = new int[row][col];
+        //swap diagonals
+        for(int i =0; i< row; i++){
+            for(int j =0; j<i; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
         }
-        return new_matrix;
+        
+       // swap values at each column/horizontals
+        int n = mat.length;
+        for(int i =0; i< row; i++){
+            for(int j =0; j<n-1-j; j++){
+               int temp = mat[i][n-1-j];
+                mat[i][n-1-j] = mat[i][j];
+                mat[i][j] = temp;
+            }
+        }
+        //put values in new matrix
+        // for(int i =0; i< row; i++){
+        //     for(int j =0; j<col; j++){
+        //         n_matrix[i][j] = mat[i][j];
+        //     }
+        // }
+        // return n_matrix;
     }
-
     
     
 }
