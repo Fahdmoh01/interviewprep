47269c7471205bc8594aa55efbedd4f095a3e949
Fahd Mohammed <34211858+Fahdmoh01@users.noreply.github.com>
2022-01-29 19:08:19 +0000

    Time: 1 ms (43.24%), Space: 45.3 MB (6.46%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int fskipper=0;
-        int[] arr = new int[nums.length];
-        for(int i =0; i<n; i ++){
-            arr[fskipper] = nums[i];
-            fskipper+=2;
+        int spointer= 0;
+        int arr[] = new int[nums.length];
+        for(int i = 0; i< nums.length / 2; i++){
+            arr[spointer] = nums[i];
+            spointer = spointer + 2;
         }
         
-        int sskipper = 1;
-        for(int i =n; i<nums.length; i ++){
-            arr[sskipper] = nums[i];
-            sskipper+=2;
+        int fpointer = 1;
+        for(int i= nums.length /2; i < nums.length; i++){
+            arr[fpointer] = nums[i];
+           fpointer = fpointer + 2;
         }
-        
         return arr;
     }
+    
+    //TC= O(N) SC= O(N)
 }
