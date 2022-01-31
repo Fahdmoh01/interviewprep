9f6c75abc19dff826fcdb1648200b0d10ffced59
Fahd Mohammed <34211858+Fahdmoh01@users.noreply.github.com>
2022-01-31 19:51:38 +0000

    Time: 0 ms (100.00%), Space: 42.3 MB (5.05%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,26 +1,22 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        //put element at index. if there is an elements shift all elements to the right and insert
-        int arr[] = new int [nums.length];
-        for(int i=0; i< arr.length; i++){
-            arr[i] = -1;
-        }
+        int t[] = new int[nums.length];
+        Arrays.fill(t, -1);
         
-        for(int i =0; i < arr.length; i++){
-            if(arr[index[i]] == -1){
-                arr[index[i]] = nums[i];
+        for(int i = 0; i< nums.length; i++){
+            if(t[index[i]] != -1){
+               shift(index[i], t);
+               t[index[i]]= nums[i];
             }else{
-                performShift(arr, index[i]);
-                arr[index[i]] = nums[i];
-
+                t[index[i]]= nums[i];    
             }
+            
         }
-        
-        return arr;
+        return t;
     }
     
-    void performShift(int arr[], int pos){
-        for(int i = arr.length - 1; i > pos; i--){
+    void shift(int pos, int arr[]){
+        for(int i = arr.length - 1; i>pos; i--){
             arr[i] = arr[i - 1];
         }
     }
