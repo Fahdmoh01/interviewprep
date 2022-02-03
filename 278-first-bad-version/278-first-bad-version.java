af257daf586680e84b18375da523349643a4acff
Fahd Mohammed <34211858+Fahdmoh01@users.noreply.github.com>
2022-02-03 10:44:32 +0000

    Time: 16 ms (56.96%), Space: 39.1 MB (7.88%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -9,11 +9,11 @@ public class Solution extends VersionControl {
         //if(n<=2 && isBadVersion(n) == true) return n;
         
         int first = search( n, true);
-        int only = search( n, false);
+//         int only = search( n, false);
         
-        if(first == -1){
-            return only;
-        }
+//         if(first == -1){
+//             return only;
+//         }
         
         return first;
     }
@@ -21,23 +21,18 @@ public class Solution extends VersionControl {
     int search( int num, boolean first){
         int start  = 1;
         int end = num;
-        int ans = -1;
+        //int ans = -1;
         while(start < end){
             int mid = start + (end - start)/2;
             if(isBadVersion(mid) == false){
                 start = mid + 1;
             }else{
                 if(isBadVersion(mid) == true){
-                    ans = mid;
-                }
-                if(first){
                     end = mid;
-                }else{
-                    start = mid + 1;
                 }
             }
         }
-        
+
         return start;
     }
 }
