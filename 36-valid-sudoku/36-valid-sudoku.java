1801582b6736b309da05e63878a1d3bead61cfff
Fahd Mohammed <34211858+Fahdmoh01@users.noreply.github.com>
2022-02-10 10:35:05 +0000

    Time: 8 ms (42.65%), Space: 48.2 MB (5.43%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.java
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -1,35 +1,35 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int n = board.length;
-        Set<Integer>[] rows = new Set[n];
-        Set<Integer>[] cols = new Set[n];
-        Set<Integer>[] blocks = new Set[n];
+       //create an array of HashSets of size n which is going to contain hashsets.
+        int n = board.length; 
+        Set<Integer>[] row = new Set[n];
+        Set<Integer>[] col = new Set[n];
+        Set<Integer>[] block = new Set[n];
         
-        for(int i = 0; i < n; i++) {
-            rows[i] = new HashSet<Integer>();
-            cols[i] = new HashSet<Integer>();
-            blocks[i] = new HashSet<Integer>();
+        for(int i = 0; i< n; i++){
+            row[i] = new HashSet<Integer>();
+            col[i] = new HashSet<Integer>();
+            block[i] = new HashSet<Integer>();
         }
-            
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if(board[i][j] != '.'){
-                    int k = 3 * (i /3) + (j / 3);
-                    int num = board[i][j];
-                    if (rows[i].contains(num)) return false;
-                    if (cols[j].contains(num)) return false;
-                    if (blocks[k].contains(num)) return false;
-                    rows[i].add(num);
-                    cols[j].add(num);
-                    blocks[k].add(num);
-                }                                 
-            }
-        }                
-        return true;
-    }
-        
-        
+        //now check the rows,columns, and blocks for the repeated number
+         for(int i =0; i< n; i++){
+             for(int j =0; j< n; j++){
+                 if(board[i][j] != '.'){
+                     //determine which block to validate.
+                     int k = 3 * (i/3) + (j/3);
+                     int num = board[i][j];
+                     if(row[i].contains(num))return false;
+                     if(col[j].contains(num))return false;
+                     if(block[k].contains(num))return false;
+                     
+                     row[i].add(num);
+                     col[j].add(num);
+                     block[k].add(num);
+                 }
+             }
+         }
+     return true;   
+    }        
 }
 
     
